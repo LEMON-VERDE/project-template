8c83af494ebc3c386fd7e642679457ea2583e6e7
Simon Thériault <thes2907@usherbrooke.ca>
2017-12-14 12:36:22 -0500

    Added Maze class

---
+++ ConsoleApplication1/Maze.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Maze
+    {
+        int[][] grid;
+
+        Coordinates startPosition;
+        Coordinates endPosition;
+
+        public int valueAt(uint x, uint y)
+        {
+            int value;
+
+            value = grid[x][y];
+
+            return value;
+        }
+        public int valueAt(Coordinates position)
+        {
+            int value;
+
+            value = valueAt(position.x, position.y);
+
+            return value;
+        }
+
+        public class Coordinates
+        {
+            public uint x;
+            public uint y;
+
+            public Coordinates(uint x, uint y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+    }
+}
