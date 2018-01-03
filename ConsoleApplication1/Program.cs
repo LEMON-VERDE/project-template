29db95acc4d50cef3194d161fdf93b93a1f58f14
Simon Thériault <thes2907@usherbrooke.ca>
2018-01-03 15:59:37 -0500

    J'ai mis un while boiiiiiiiiiiiiii

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,10 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            while(true)
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
