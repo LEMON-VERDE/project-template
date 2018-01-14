c9494167c7f1a5703393e3903382141fb80acfef
MoronixProduct3 <moronicproduct3@gmail.com>
2018-01-14 11:04:25 -0500

    Revert "J'ai mis un while boiiiiiiiiiiiiii"
    This reverts commit 29db95acc4d50cef3194d161fdf93b93a1f58f14.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,10 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            while(true)
-            {
-                Console.ReadKey();
-            }
+            Console.ReadKey();
         }
     }
 }
